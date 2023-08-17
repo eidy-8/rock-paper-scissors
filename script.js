5b81541c3893d878d2fb70e219e38b06f38660f3
enrique <enriqueonaga@gmail.com>
2023-08-16 22:02:40 -0300

    develop the playround function

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
+    const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
 
-const computerChoice = getComputerChoice();
-console.log("Computer's choice: " + computerChoice);
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return ("Draw")
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return ("You Lost!")
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return ("You Won!")
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return ("You Won!")
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return ("You Lost!")
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return ("You Won!")
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return ("You Lost!")
+    }
+  }
+   
+const playerSelection = prompt("Enter a value: ");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+console.log("Your Choice: " + playerSelection + "\n" + "Computer's Choice: " + computerSelection);
+
+
